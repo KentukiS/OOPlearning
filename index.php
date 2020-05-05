c9ea140d21c09c716547050dbfb8b161cfcf16cc
Kentuki <dmitriy.streliany@gmail.com>
2020-05-05 16:04:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Room
+{
+	public $color = 'red'; 
+
+	public function getColor()
+	{
+		echo $this->color;
+	}
+
+	public function changeColor($color)
+	{
+		echo $this->color;
+		$this->color = $color ;
+	}
+}
+
+$object = new Room();
+$object->changeColor("green");
+$object->getColor();
